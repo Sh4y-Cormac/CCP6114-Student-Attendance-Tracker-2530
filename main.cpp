cc5b8821d1cd2d1bf3056d8f482aec844052131e
Umar <umarzaidtenby@gmail.com>
2025-12-22 10:57:18 +0800

    Adding number of columns input

--- main.cpp
+++ main.cpp
@@ -1,11 +1,28 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
-int main(}
+void DisplayText(string); // this function is used to display text onto the command line with endline aswell
+void create_sheet_structure(); // this function handles the sheet structure creation
+int number_of_columns;
+
+
+int main()
 {
-    // testing if i can push code from codeblocks lol
-    int var;
-    var = 10;
-    cout << var << endl;
+    create_sheet_structure();
     return 0;
 }
+
+
+void DisplayText(string Text)
+{
+    cout << Text << endl;
+}
+
+void create_sheet_structure()
+{
+    DisplayText("Define number of columns(max 10): ");
+    cin >> number_of_columns;
+}
+
