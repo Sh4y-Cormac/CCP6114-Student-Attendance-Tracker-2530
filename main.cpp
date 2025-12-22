ef399f2d5b3b9f8568119ce2a8768a6aec1948bc
Umar <umarzaidtenby@gmail.com>
2025-12-22 11:33:09 +0800

    adding comments

--- main.cpp
+++ main.cpp
@@ -7,14 +7,14 @@ void create_sheet_structure(); // this function handles the sheet structure crea
 
 
 int number_of_columns;
-string column_names[10] = {"John","Jeffrey","Jack"};
+string column_names[10] = {}; // makes an array of all the columns
 
 
 
 
 int main()
 {
-    create_sheet_structure();
+    create_sheet_structure(); // create sheet structure
     return 0;
 }
 
@@ -23,7 +23,7 @@ void create_sheet_structure()
     cout << "Define number of columns (max 10): " << endl;
     cin >> number_of_columns;
 
-    for (int x=0; x<number_of_columns; x++)
+    for (int x=0; x<number_of_columns; x++) // loops through the number of columns and receives input for the names of the column
     {
         string name;
         cout << "Enter column " << x+1 << " name: " << endl;
