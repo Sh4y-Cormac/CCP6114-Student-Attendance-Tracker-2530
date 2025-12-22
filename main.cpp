6dcc2362eb313866f6becaf9c3c241c4cb9e782c
Umar <umarzaidtenby@gmail.com>
2025-12-22 11:29:50 +0800

    Implementing inputs for number of columns

--- main.cpp
+++ main.cpp
@@ -3,9 +3,13 @@
 
 using namespace std;
 
-void DisplayText(string); // this function is used to display text onto the command line with endline aswell
 void create_sheet_structure(); // this function handles the sheet structure creation
+
+
 int number_of_columns;
+string column_names[10] = {"John","Jeffrey","Jack"};
+
+
 
 
 int main()
@@ -14,15 +18,21 @@ int main()
     return 0;
 }
 
-
-void DisplayText(string Text)
-{
-    cout << Text << endl;
-}
-
 void create_sheet_structure()
 {
-    DisplayText("Define number of columns(max 10): ");
+    cout << "Define number of columns (max 10): " << endl;
     cin >> number_of_columns;
+
+    for (int x=0; x<number_of_columns; x++)
+    {
+        string name;
+        cout << "Enter column " << x+1 << " name: " << endl;
+        cin >> name;
+        column_names[x] = name;
+
+    }
+
 }
+
+
 
