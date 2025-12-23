4e71723768491ff006f6001611221ae34a64f67d
Umar <umarzaidtenby@gmail.com>
2025-12-23 15:26:55 +0800

    adding attendance row creation

--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 using namespace std;
 
 void create_sheet_structure(); // this function handles the sheet structure creation
+void create_attendance_row();
 
 
 int number_of_columns;
+int attendance_row_value;
 string column_names[10] = {"John","Jeffrey","Jack"};
-
 
 
 
@@ -23,14 +24,24 @@ void create_sheet_structure()
     cout << "Define number of columns (max 10): " << endl;
     cin >> number_of_columns;
 
-    for (int x=0; x<number_of_columns; x++)
-    {
+    for (int x=0; x<number_of_columns; x++) // iterates through the specified number of columns, asks for column name for each one and
+    {                                       // assigning it to an index inside an array according to order
         string name;
         cout << "Enter column " << x+1 << " name: " << endl;
         cin >> name;
         column_names[x] = name;
 
     }
+
+    cout << "Sheet structure created successfully" << endl;
+
+    do
+    {
+        cout << "Press 1 for a new attendance row, Press 2 to exit attendance rows" << endl;
+        cin >> attendance_row_value;
+
+
+    } while (attendance_row_value == 1);
 
 }
 
