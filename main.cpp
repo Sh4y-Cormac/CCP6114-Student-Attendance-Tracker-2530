ed80aaa21c92f3ecc69e60a3562bde520539a0a9
Umar <umarzaidtenby@gmail.com>
2025-12-23 16:05:13 +0800

    adding new attendance row iteration

--- main.cpp
+++ main.cpp
@@ -10,6 +10,9 @@ void create_attendance_row();
 int number_of_columns;
 int attendance_row_value;
 string column_names[10] = {"John","Jeffrey","Jack"};
+string column1_data[10] = {};
+string column2_data[10] = {};
+string column3_data[10] = {};
 
 
 
@@ -25,7 +28,7 @@ void create_sheet_structure()
     cin >> number_of_columns;
 
     for (int x=0; x<number_of_columns; x++) // iterates through the specified number of columns, asks for column name for each one and
-    {                                       // assigning it to an index inside an array according to order
+    {                                       // assigs it to an index inside an array according to order
         string name;
         cout << "Enter column " << x+1 << " name: " << endl;
         cin >> name;
@@ -39,9 +42,34 @@ void create_sheet_structure()
     {
         cout << "Press 1 for a new attendance row, Press 2 to exit attendance rows" << endl;
         cin >> attendance_row_value;
+        create_attendance_row();
 
 
     } while (attendance_row_value == 1);
+
+}
+
+void create_attendance_row()
+{
+    cout << "------------------------" << endl;
+    cout << "Insert new attendance row" << endl;
+    cout << "------------------------" << endl;
+
+    for (int x=0; x<number_of_columns; x++)
+    {
+        string temp = "column";
+        string index = to_string(x);
+        string temp2 = temp.append(index);
+        string selection_data = temp2.append("_data");
+
+        cout << "Enter " << column_names[x] << ":";
+        cin >> selection_data;
+    }
+
+    cout << column1_data[0] << endl;
+    cout << column2_data[0] << endl;
+    cout << column3_data[0]
+
 
 }
 
