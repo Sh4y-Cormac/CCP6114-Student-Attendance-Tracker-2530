315b86c5c56604a80b7a64e7cdeee394306f51a8
Wilson <ongjunze287@gmail.com>
2026-01-04 23:26:30 +0800

    merged

--- main.cpp
+++ main.cpp
@@ -240,7 +240,7 @@ void displayMainMenu() {
     cout << "2. Load existing attendance sheet" << endl;
     cout << "3. Logout\n"
          << endl;
-    cout << "Type 1 to create new attendance sheet, 2 to load existing attendance sheet or 3 to logout: ";
+    cout << "Type 1 to create a new attendance sheet, 2 to load an existing attendance sheet, or 3 to logout: ";
 }
 
 int main_menu()
